4773eff738cb3e5b84566195486b6603bd31005f
Gabriel Barros <gabrielbarros@Gabriels-MacBook-Pro.local>
2018-02-03 19:02:57 -0500

    Initial Commit

---
+++ hw1_2/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  hw1_2
+//
+//  Created by Gabriel Barros on 2/3/18.
+//  Copyright © 2018 Gabriel Barros. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
